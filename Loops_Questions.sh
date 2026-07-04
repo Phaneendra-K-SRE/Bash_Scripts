9532f979d5f11f3f50d34bb949727097fe3d907d
Phaneendra-K-SRE <phaneendrakavala@gmail.com>
2026-07-05 04:42:49 +0530

    Question 2

--- Loops_Questions.sh
+++ Loops_Questions.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-printNumbers={1..10}
+# "This script will print the value of i from 1 to 10 and determine if it is even or odd."
 
-for number in $printNumbers
+for i in {1..10}
 do
-    echo $number
+  echo "prints the value of i: $i"
+
+  if ((i % 2 == 0)); then
+    echo "$i is even"
+  else
+    echo "$i is odd"
+  fi
+  sleep 3
 done
