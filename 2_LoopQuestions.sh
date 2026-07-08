65ea5496af9d96f9930cf484433a55be172d97af
Phaneendra-K-SRE <phaneendrakavala@gmail.com>
2026-07-08 19:00:11 +0530

    Checking tools

---
+++ 2_LoopQuestions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#"this script will check and print each tool"
+
+counter=0
+
+for tool in nginx docker jenkins terraform
+do
+  echo "checking tool: $tool"
+  ((counter++));
+   echo "total tools checked: $counter"
+  fi
+  sleep 3
+done    
